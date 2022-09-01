17ba669e15f474f597fc107deb5f40f6d32e65d8
Nalin Padmasiri <nalinsasika9@gmail.com>
2022-09-01 22:38:28 +0800

    api-doc init

---
+++ api/paths/auth/login/index.js
@@ -0,0 +1,98 @@
+module.exports = function () {
+    let operations = {
+      GET,
+      POST,
+      PUT,
+      DELETE,
+    };
+  
+    function GET(req, res, next) {
+      res.status(200).json([
+        { id: 0, message: "First todo" },
+        { id: 1, message: "Second todo" },
+      ]);
+    }
+  
+    function POST(req, res, next) {
+      console.log(`About to create todo: ${JSON.stringify(req.body)}`);
+      res.status(201).send();
+    }
+  
+    function PUT(req, res, next) {
+      console.log(`About to update todo id: ${req.query.id}`);
+      res.status(200).send();
+    }
+  
+    function DELETE(req, res, next) {
+      console.log(`About to delete todo id: ${req.query.id}`);
+      res.status(200).send();
+    }
+  
+  
+    POST.apiDoc = {
+      summary: "Login User",
+      operationId: "createTodo",
+      consumes: ["application/json"],
+      parameters: [
+        {
+          in: "body",
+          name: "todo",
+          schema: {
+            $ref: "#/definitions/Todo",
+          },
+        },
+      ],
+      responses: {
+        201: {
+          description: "Created",
+        },
+      },
+    };
+  
+    PUT.apiDoc = {
+      summary: "Update todo.",
+      operationId: "updateTodo",
+      parameters: [
+        {
+          in: "query",
+          name: "id",
+          required: true,
+          type: "string",
+        },
+        {
+          in: "body",
+          name: "todo",
+          schema: {
+            $ref: "#/definitions/Todo",
+          },
+        },
+      ],
+      responses: {
+        200: {
+          description: "Updated ok",
+        },
+      },
+    };
+  
+    DELETE.apiDoc = {
+      summary: "Delete todo.",
+      operationId: "deleteTodo",
+      consumes: ["application/json"],
+      parameters: [
+        {
+          in: "query",
+          name: "id",
+          required: true,
+          type: "string",
+        },
+      ],
+      responses: {
+        200: {
+          description: "Delete",
+        },
+      },
+    };
+  
+    return operations;
+  };
+  
